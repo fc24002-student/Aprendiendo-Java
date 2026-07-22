6158312e4ffb93a575db0a1edb7f5a474c16ed38
José Eduardo Flores Cortez <fc24002@ues.edu.sv>
2026-07-21 19:48:36 -0600

    feat: Ejercicios de variablesAndConstants and DataTypes BegginerExercises

--- src/BegginerExercises.java
+++ src/BegginerExercises.java
@@ -3,26 +3,137 @@ public class BegginerExercises {
 //EJERCICIOS VARIABLES Y CONSTANTES
 
     public static void main(String[] args) {
+        Ejercicio1.Terminal1();
+        Ejercicio2.Terminal2();
+        Ejercicio3.Terminal3();
+        Ejercicio4.Terminal4();
+        Ejercicio5.Terminal5();
+        Ejercicio6.Terminal6();
+        Ejercicio7.Terminal7();
+        Ejercicio8.Terminal8();
+        Ejercicio9.Terminal9();
+        Ejercicio10.Terminal10();
 
     }
 }
 
 // 1. Declara una varables de tipo String y asignale tu nombre.
 
+class Ejercicio1 {
+    public static void Terminal1() {
+        String name = "Flores";
+        System.out.println(name);
+    }
+}
+
 // 2. Crea una vaiabe de tipo int y asignale tu edad.
+
+class Ejercicio2 {
+    public static void Terminal2() {
+        int age = 20;
+        System.out.println(age);
+    }
+}
 
 // 3.  Declara una variable double con tu altura en metros.
 
+class Ejercicio3 {
+    public static void Terminal3() {
+        double altura = 1.71;
+        System.out.println(altura);
+
+    }
+}
+
 // 4. Declara una variable de tipo boolean si te gusta programar
+
+class Ejercicio4 {
+    public static void Terminal4() {
+        boolean siMeguta = true;
+        System.out.println("Te gusta Programar? " + siMeguta);
+    }
+}
 
 // 5. Declara una constante con tu email.
 
+class Ejercicio5 {
+    public static void Terminal5() {
+        final String email = "universitario.123@gmail.com";
+        System.out.println(email);
+    }
+}
+
 // 6. Crea una variable de tiṕo char y guárdale tu inicial.
+
+class Ejercicio6 {
+    public static void Terminal6() {
+        char inicialName = 'J';
+        System.out.println(inicialName);
+    }
+}
 
 // 7. Declara una variable de tipo String con tu localidad y a continuación cambia su valor y vuelve a imprimirla
 
+class Ejercicio7 {
+    public static void Terminal7() {
+        String localidad = "Localidad1";
+        localidad = "LOCALIDAD2";
+        System.out.println(localidad);
+    }
+}
+
 // 8. Crea una variable int llamada a, otra b, e imnprime la suma de ambas.
+
+class Ejercicio8 {
+    public static void Terminal8() {
+        int a = 3;
+        int b = 2;
+        System.out.println("Valor a: " + a);
+        System.out.println("Valor b: " + b);
+        int suma = a + b;
+        System.out.println("La suma de a + b es: " + suma);
+    }
+}
 
 // 9. Imprime el tipo de dos variables creadas anteriormente.
 
+class Ejercicio9 {
+    public static void Terminal9() {
+
+        String localidad = "Localidad1";
+        System.out.println(localidad.getClass().getSimpleName());
+
+        Double altura = 1.71;
+        System.out.println(altura.getClass().getSimpleName());
+
+    }
+}
+
 // 10. Intenta declarar una variable sin inicializarla y luego asignale un valor antes de imprimirla.
+
+class Ejercicio10 {
+    public static void Terminal10() {
+
+        int metros; //VARIABLE INICIALIZADA SIN VALOR
+
+        metros = 30; //LE ASIGNAMOS VALOR A LA VARIABLE = 30
+
+        System.out.println(metros + " m"); //IMPRIMIMOS LA VARIABLE YA CON DATO
+
+        /*
+        SI TRATAMOS DE IMPRIMIR UNA VARIABLE SIN INICIALIZARLA NO COMPILA
+        java: cannot find symbol
+        symbol:   variable metros
+        location: class Ejercicio10
+
+        SI IMPRIMIMOS LA VARIABLE SIN VALOR SOLO INICIALIZADA PASA ESTO:
+        java: variable metros might not have been initialized
+        (Traducción: "La variable metros podría no haber sido inicializada")
+
+         * NOTA TÉCNICA:
+         En Java, las variables locales DEBEN tener un valor asignado antes de ser
+         leídas o impresas. Si intentamos usarlas sin inicializarlas previamente,
+         el compilador dará el error: "variable might not have been initialized".
+         */
+    }
+}
--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,12 +1,12 @@
 public class DataTypes {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         //TIPOS DE DATOS PRIMITIVOS
         //int = ENTERO
         int myInt = 37;
         System.out.println(myInt);
 
-        //double = DECIMALES
+        //double = DECIMALES CON ALTA PRECISIÓN
         double myDouble = 1.70;
         System.out.println(myDouble);
 
@@ -22,18 +22,61 @@ public class DataTypes {
         myBoolean = false;
         System.out.println(myBoolean);
 
-        //OTROS DATOS PRIMITIVOS SON; float, long, byte
+        //OTROS DATOS PRIMITIVOS SON; float, long, short, byte
 
-        //long = MAS LARGO QUE DOUBLE
-        //float = MAS CORTO QUE DOUBLE
+        //long = ES PARA NUMEROS ENTEROS SIN DECIMALES MUY GRANDES
+        //float = MAS CORTO QUE DOUBLE TIENE MENOS DECIMALES QUE DOUBLE Y TIENE MENOS PRECISIÓN
 
-        float myFloat = '2';
+        /*
+        float (Decimal de precisión simple)
+Tamaño en RAM: 4 bytes (32 bits).
+
+Precisión: De 6 a 7 dígitos decimales.
+
+¿Para qué sirve?: Para números decimales donde no necesitas tanta precisión matemática o científica (por ejemplo,
+gráficos 3D, coordenadas de videojuegos, porcentajes básicos).
+
+Sintaxis especial: Por defecto, cualquier número con decimal en Java es tratado como double. Para indicarle a Java que
+sea float, debes ponerle una f o F al final.
+
+Ejemplo correcto: float precio = 19.99f;
+         */
+
+        float myFloat = 2.5f;
         System.out.println(myFloat);
 
-        long myLong = '2';
+        /*
+        long (Entero muy grande)
+Tamaño en RAM: 8 bytes (64 bits).
+
+Rango: De -9,223,372,036,854,775,808 a 9,223,372,036,854,775,807.
+
+¿Para qué sirve?: Para valores enteros que sobrepasan el límite de un int (~2,000 millones). Por ejemplo: fechas en
+milisegundos (System.currentTimeMillis()), identificadores únicos mundiales (UUID/IDs de base de datos grandes) o
+distancias astronómicas.
+
+Sintaxis especial: Debes ponerle una L al final del número para que Java sepa que es de tipo long.
+
+Ejemplo correcto: long poblacionMundial = 8000000000L;
+         */
+
+        long myLong = 20000000000L;
         System.out.println(myLong);
 
-        byte myByte = '2';
+        /*
+        //byte = ES UN ENTERO MUY PEQUEÑO EN RAM 1 byte (SON 8 bits)
+        byte (Entero muy pequeño)
+Tamaño en RAM: 1 byte (8 bits).
+
+Rango: De -128 a 127.
+
+¿Para qué sirve?: Se usa cuando necesitas ahorrar memoria al máximo y procesas miles o millones de datos pequeños
+(por ejemplo, en transmisión de datos por red, procesamiento de imágenes o manipulación de archivos binarios).
+
+Ejemplo correcto: byte edad = 2
+         */
+
+        byte myByte = 2;
         System.out.println(myByte);
 
         //String = ES UN DATO PRIMITIVO POR SER CADENA DE TEXTO PEROO ES MAS UNA CLASE OSEA UN OBJETO YA QUE SE USA MUCHO
@@ -52,5 +95,5 @@ public class DataTypes {
         //RESULTADO EN TERMINAL ES String esto se hace al tener una clase ESTO ES UNA COMPROBACIÓN EN TIEMPOS DE EJECUCIÓN
 
 
-        }
+    }
 }
